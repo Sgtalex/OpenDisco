ec7f227ebdf7de684dfba3d747505a2ef908af65
WolfHex <94994488+WolfHex@users.noreply.github.com>
2021-11-26 21:29:07 +0100

    Add files via upload

---
+++ OpenDisco.py
@@ -0,0 +1,183 @@
+#So cool stuff
+import os
+import requests, threading, os , sys
+from os import system
+
+try:
+    import colorama
+except:
+    os.system("py -3 -m pip install colorama")
+    import requests
+
+
+try:
+    import ctypes
+except:
+    os.system("py -3 -m pip install ctypes")
+    import requests
+
+try:
+    import discord
+except:
+    os.system("py -3 -m pip install discord")
+    import requests
+
+try:
+    import threading
+except:
+    os.system("py -3 -m pip install threading")
+    import requests
+
+try:
+    import selenium
+except:
+    os.system("py -3 -m pip install selenium")
+    import requests
+
+try:
+    import webdriver_mananger
+except:
+    os.system("py -3 -m pip install pip install webdriver-manager")
+    import requests
+
+try:
+    import asyncio
+except:
+    os.system("py -3 -m pip install asyncio")
+
+
+try:
+    import bs4
+except:
+    os.system("py -3 -m pip install bs4")
+ 
+
+
+import time
+from time import sleep
+from colorama import Fore, Style, init
+
+from discord.ext import commands
+import threading
+import discord
+
+system("title " + "OpenDisco")
+
+init()
+
+print('')
+print('')
+print('')
+print('')
+print('')
+print('')
+print('')
+print('')
+print(Fore.GREEN + '  .88888.                             888888ba  oo           ')
+time.sleep( 0.2)
+print(Fore.GREEN + ' d8     8b                            88     8b                               ')
+time.sleep( 0.2)
+print(Fore.GREEN + ' 88     88 88d888b. .d8888b. 88d888b. 88     88 dP .d8888b. .d8888b. .d8888b. ')
+time.sleep( 0.2)
+print(Fore.WHITE + ' 88     88 88    88 88ooood8 88    88 88     88 88 Y8ooooo. 88       88    88 ')
+time.sleep( 0.2)
+print(Fore.WHITE + ' Y8.   .8P 88.  .88 88.  ... 88    88 88    .8P 88       88 88.  ... 88.  .88 ')
+time.sleep( 0.2)
+print(Fore.RED + '   8888P   88Y888P   88888P  dP    dP 8888888P  dP  88888P   88888P   88888P  ')
+time.sleep( 0.2)
+print(Fore.RED + '           88                                                                 ')
+time.sleep( 0.2)
+print(Fore.RED + '           dP                       ')
+time.sleep( 0.2)
+
+time.sleep( 0.2) 
+print(' ')
+print(Fore.RED + '                                                                    Made by WolfHex')
+
+print(Fore.YELLOW+"""
+╔═════════════════════════╦══════════════════════════╗
+║ 1.Account-Generator     ║  Nothing                 ║   
+║ 2.Token-Bruteforce      ║  Nothing                 ║       
+║ 3.Discord-Nuker         ║  Nothing                 ║
+║ 4.Proxy-Generator       ║  Nothing                 ║
+║ 5.Discord-Token-Nuke    ║  Nothing                 ║
+║ 6.Account-Nuker         ║  Nothing                 ║
+║ 7.NitroGen-Check        ║  Nothing                 ║
+╚═════════════════════════╩══════════════════════════╝
+""")
+
+command = input('bash@OpenDisco:~$ ')
+
+
+if command == '1':
+   os.system('cmd /k "py -3 data/accgen/accountgen.py"')
+
+elif command == '2':
+  os.system('cmd /k "py -3 data/tokengen/tkg.py')
+
+elif command == '3':
+   os.system('cmd /k "py -3 data/nuke/nuke.py')
+
+elif command == '4':
+    os.system('cmd /k "py -3 data/prxgen/prxgen.py')
+
+elif command == '5':
+    os.system('cmd /k "python phlocator.py"')
+
+elif command == '6':
+    os.system('cmd /k "py -3 data/dank/dsna.py')
+
+elif command == '7':
+    os.system('cmd /k "py -3 data/nitrogencheck/ngc.py')
+
+elif command == '8':
+    os.system('cmd /k ""')
+
+elif command == '9':
+    os.system('cmd /k ""')
+
+
+elif command == '10':
+    os.system('cmd /k ""')
+
+elif command == '11':
+    os.system('cmd /k ""')
+
+
+elif command == '12':
+    os.system('cmd /k ""')
+
+elif command == '13':
+  os.system('cmd /k ""')
+
+elif command == '14':
+  os.system('cmd /k ""')
+    
+      
+
+else:
+  print('init: Command not found')
+  print(' ')
+  print(' ')
+  print(' ')
+  print(' ')
+  print(' ')
+  print(' ')
+  print(' ')
+  print(' ')
+  print(' ')
+  print(' ')
+  print(' ')
+  print(' ')
+  print(' ')
+  print(' ')
+  print(' ')
+  print(' ')
+  print(' ')
+  os.system('cmd /k "py -3 OpenDisco.py"')
+
+
+  
+
+
+   
